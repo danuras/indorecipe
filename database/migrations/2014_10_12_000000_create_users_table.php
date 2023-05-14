d993a08e6d91febafc85256d24e2ce9d387dc8da
Darussalaam Nur Rasyidu <darussalaamnurrasyidu@gmail.com>
2023-05-14 08:11:54 +0700

    perbaikan

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('username')->unique();
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('info')->nullable();
+            $table->string('bio')->nullable();
+            $table->string('verification_code')->nullable();
+            $table->timestamp('verification_code_expired_at')->nullable();
+            $table->binary('photo_profile') -> nullable();
+            $table->string('no_phone')->nullable();
+            $table->string('gender')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
